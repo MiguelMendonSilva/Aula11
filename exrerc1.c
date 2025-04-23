f1cd3bc108893416d07bbc4cdc68fb096b6fd18d
Miguel <124186796+MiguelMendonSilva@users.noreply.github.com>
2025-04-23 19:46:13 -0300

    Update exrerc1.c

--- exrerc1.c
+++ exrerc1.c
@@ -2,20 +2,11 @@
 
 int main(){
 
-    int notas[5];
-
-    notas[0] = 1;
-    notas[1] = 2;
-    notas[2] = 3;
-    notas[3] = 4;
-    notas[4] = 5;
+    int notas[5] = {1, 2, 3, 4, 5};
 
     for (int n = 0; n < 5; n++){
         printf("notas[%d]: %d.\n", n, notas[n]);
     }
-
-
-
 
 
     return 0;
