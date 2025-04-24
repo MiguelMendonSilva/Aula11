747704bb55d5d815dcedb49c103290fee1ece410
Miguel <124186796+MiguelMendonSilva@users.noreply.github.com>
2025-04-23 21:47:18 -0300

    Create exerc5.c

---
+++ exerc5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void){
+
+    float numeros[2];
+
+    printf("Digite um numero: ");
+    scanf("%f", &numeros[0]);
+
+    printf("Digite outro numero: ");
+    scanf("%f", &numeros[1]);
+
+    printf("Valor 1: %f\n", numeros[0]);
+    
+
+    return 0;
+
+}
+
+
